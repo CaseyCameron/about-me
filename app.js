2ee435e99a64ff2d80e53ce12f4213960f77bd39
CaseyCameron <mcasey.cameron@gmail.com>
2022-04-20 14:26:22 -0700

    CI fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // grab DOM elements
-const button = document.getElementById('button');
-const nameEl = document.getElementById('name');
-const sectionEl = document.getElementById('my-info');
+// const button = document.getElementById('button');
+// const nameEl = document.getElementById('name');
+// const sectionEl = document.getElementById('my-info');
 const animalDiv = document.getElementById('animal-div');
 
 const animalButton = document.getElementById('animal-button');
